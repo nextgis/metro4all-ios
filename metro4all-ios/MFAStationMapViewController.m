ade82c7c2d1d9ecd7f55cc55465afd0a7b049979
Maxim Smirnov <max.fleshy@gmail.com>
2015-03-11 14:44:09 +0300

    Zoom and pan station scheme Fix #10

--- metro4all-ios/MFAStationMapViewController.m
+++ metro4all-ios/MFAStationMapViewController.m
@@ -16,8 +16,8 @@
 @property (nonatomic, weak) IBOutlet MKMapView *mapView;
 
 @property (nonatomic, weak) IBOutlet UIScrollView *schemeScrollView;
-@property (nonatomic, weak) IBOutlet UIImageView *schemeView;
-@property (nonatomic, weak) IBOutlet UIImageView *schemeOverlayView;
+@property (nonatomic, weak) UIView *containerView;
+@property (nonatomic, weak) UIImageView *schemeOverlayView;
 
 @property (nonatomic, weak) IBOutlet UISegmentedControl *modeSwitch;
 
@@ -39,11 +39,7 @@
     
     RAC(self.mapView, hidden) = [showsMapSignal not];
     
-    RAC(self.schemeView, hidden) = showsMapSignal;
-    RAC(self.schemeView, image) = RACObserve(self.viewModel, stationSchemeImage);
-    
-    RAC(self.schemeOverlayView, image) = RACObserve(self.viewModel, stationSchemeOverlayImage);
-    RAC(self.schemeOverlayView, hidden) = [RACObserve(self.viewModel, showsObstacles) not];
+    RAC(self.schemeScrollView, hidden) = showsMapSignal;
     
     RAC(self.modeSwitch, selectedSegmentIndex) =
         [RACObserve(self.viewModel, showsMap) map:^id(NSNumber *value) {
@@ -62,6 +58,22 @@
                           reduce:^NSNumber *(NSNumber *showsMap, NSNumber *showsPortals, NSNumber *showsObstacles) {
                               return showsMap.boolValue ? showsPortals : showsObstacles;
                           }];
+    
+    UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                action:@selector(zoomImage:)];
+    doubleTap.numberOfTapsRequired = 2;
+    doubleTap.numberOfTouchesRequired = 1;
+    
+    [self.schemeScrollView addGestureRecognizer:doubleTap];
+    
+    UITapGestureRecognizer *pinch = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                action:@selector(scrollViewTwoFingerTapped:)];
+    pinch.numberOfTapsRequired = 1;
+    pinch.numberOfTouchesRequired = 2;
+    
+    [self.schemeScrollView addGestureRecognizer:pinch];
+
+    self.schemeScrollView.delegate = self;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -71,6 +83,31 @@
     CLLocationCoordinate2D stationPos = self.viewModel.stationPos;
     MKCoordinateRegion mapRegion = MKCoordinateRegionMakeWithDistance(stationPos, 500, 500);
     self.mapView.region = mapRegion;
+    
+    UIImage *schemeImage = self.viewModel.stationSchemeImage;
+    UIImageView *schemeView = [[UIImageView alloc] initWithImage:schemeImage];
+    UIImageView *overlayView = [[UIImageView alloc] initWithImage:self.viewModel.stationSchemeOverlayImage];
+    
+    UIView *container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, schemeImage.size.width, schemeImage.size.height)];
+    
+    [container addSubview:schemeView];
+    [container addSubview:overlayView];
+    
+    [self.schemeScrollView addSubview:container];
+    self.schemeScrollView.contentSize = container.frame.size;
+
+    self.schemeScrollView.contentInset = UIEdgeInsetsMake(0, 0, 64, 0);
+    
+    self.containerView = container;
+    self.schemeOverlayView = overlayView;
+    
+    RAC(self.schemeOverlayView, hidden) = [RACObserve(self.viewModel, showsObstacles) not];
+    
+    // 5
+    self.schemeScrollView.maximumZoomScale = 1.0f;
+    [self setMinimumZoomScale];
+    
+    [self centerScrollViewContents];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -87,6 +124,18 @@
             [self.mapView addAnnotation:mark];
         }
     }];
+}
+
+- (void)setMinimumZoomScale
+{
+    CGRect scrollViewFrame = self.schemeScrollView.frame;
+    
+    CGFloat scaleWidth = scrollViewFrame.size.width / self.schemeScrollView.contentSize.width;
+    CGFloat scaleHeight = scrollViewFrame.size.height / self.schemeScrollView.contentSize.height;
+    CGFloat minScale = MIN(scaleWidth, scaleHeight);
+    
+    self.schemeScrollView.minimumZoomScale = minScale;
+    self.schemeScrollView.zoomScale = minScale;
 }
 
 - (IBAction)segmentChanged:(UISegmentedControl *)sender
@@ -107,6 +156,92 @@
     else {
         self.viewModel.showsObstacles = sender.isOn;
     }
+}
+
+- (void)zoomImage:(UIGestureRecognizer *)recognizer
+{
+    // 1
+    CGPoint pointInView = [recognizer locationInView:self.schemeScrollView.subviews[0]];
+    
+    // 2
+    CGFloat newZoomScale = self.schemeScrollView.zoomScale * 2.0;
+    newZoomScale = MIN(newZoomScale, self.schemeScrollView.maximumZoomScale);
+    
+    // 3
+    CGSize scrollViewSize = self.schemeScrollView.bounds.size;
+    
+    CGFloat w = scrollViewSize.width / newZoomScale;
+    CGFloat h = scrollViewSize.height / newZoomScale;
+    CGFloat x = pointInView.x - (w / 2.0f);
+    CGFloat y = pointInView.y - (h / 2.0f);
+    
+    CGRect rectToZoomTo = CGRectMake(x, y, w, h);
+    
+    // 4
+    [self.schemeScrollView zoomToRect:rectToZoomTo animated:YES];
+}
+
+- (void)centerScrollViewContents {
+    CGSize boundsSize = self.schemeScrollView.bounds.size;
+    CGRect contentsFrame = self.containerView.frame;
+    
+    if (contentsFrame.size.width < boundsSize.width) {
+        contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2.0f;
+    } else {
+        contentsFrame.origin.x = 0.0f;
+    }
+    
+    if (contentsFrame.size.height < boundsSize.height) {
+        contentsFrame.origin.y = (boundsSize.height - contentsFrame.size.height) / 2.0f;
+    } else {
+        contentsFrame.origin.y = 0.0f;
+    }
+    
+    self.containerView.frame = contentsFrame;
+}
+
+- (void)scrollViewTwoFingerTapped:(UITapGestureRecognizer *)recognizer {
+    // Zoom out slightly, capping at the minimum zoom scale specified by the scroll view
+    CGFloat newZoomScale = self.schemeScrollView.zoomScale / 1.5f;
+    newZoomScale = MAX(newZoomScale, self.schemeScrollView.minimumZoomScale);
+    [self.schemeScrollView setZoomScale:newZoomScale animated:YES];
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    // Return the view that you want to zoom
+    return self.schemeScrollView.subviews[0];
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView {
+    // The scroll view has zoomed, so we need to re-center the contents
+    [self centerScrollViewContents];
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    [self setMinimumZoomScale];
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+{
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    
+    //The device has already rotated, that's why this method is being called.
+    UIInterfaceOrientation toOrientation   = [[UIDevice currentDevice] orientation];
+    
+    //fixes orientation mismatch (between UIDeviceOrientation and UIInterfaceOrientation)
+    if (toOrientation == UIInterfaceOrientationLandscapeRight) toOrientation = UIInterfaceOrientationLandscapeLeft;
+    else if (toOrientation == UIInterfaceOrientationLandscapeLeft) toOrientation = UIInterfaceOrientationLandscapeRight;
+    
+    UIInterfaceOrientation fromOrientation = [[UIApplication sharedApplication] statusBarOrientation];
+    
+    [self willRotateToInterfaceOrientation:toOrientation duration:0.0];
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        [self willAnimateRotationToInterfaceOrientation:toOrientation duration:[context transitionDuration]];
+    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        [self didRotateFromInterfaceOrientation:fromOrientation];
+    }];
+    
 }
 
 @end
